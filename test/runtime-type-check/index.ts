81a444866ce2d8dabd11346990b45c49c1a4877a
Jonathan Cole <coleji@bc.edu>
2019-03-24 21:40:41 -0400

    object and array validators

--- test/runtime-type-check/index.ts
+++ test/runtime-type-check/index.ts
@@ -2,35 +2,66 @@ import * as t from 'io-ts'
 import { PathReporter } from 'io-ts/lib/PathReporter'
 import * as assert from "assert"
 
+const innerObjShape = t.type({
+	innerString: t.string
+})
+
+const listObjShape = t.type({
+	listedString: t.string
+})
+
 const shape = t.type({
 	someString: t.string,
 	someNumber: t.number,
 	someBoolean: t.boolean,
-	optionalString: t.union([t.string, t.null])
+	nestedObject: innerObjShape,
+	stringList: t.array(t.string),
+	objectList: t.array(listObjShape)
+	//optionalString: t.union([t.string, t.null])
 })
 
 const ok = JSON.stringify({
-	"someString": "hi",
-	"someNumber": 1,
-	"someBoolean": true
+	someString: "hi",
+	someNumber: 1,
+	someBoolean: true,
+	stringList: ["hi", "yo", "whatup"],
+	objectList: [{listedString: "a"}, {listedString: "b"}, {listedString: "c"}],
+	nestedObject: {
+		innerString: "hi again"
+	}
 })
 
 // falsy things
 const alsoOk = JSON.stringify({
-	"someString": "",
-	"someNumber": 0,
-	"someBoolean": false
+	someString: "",
+	someNumber: 0,
+	someBoolean: false,
+	stringList: ["hi", "yo", "whatup"],
+	objectList: [{listedString: "a"}, {listedString: "b"}, {listedString: "c"}],
+	nestedObject: {
+		innerString: "hi again"
+	}
 })
 
 const missingString = JSON.stringify({
-	"someNumber": 1,
-	"someBoolean": true
+	someNumber: 1,
+	someBoolean: true,
+	stringList: ["hi", "yo", "whatup"],
+	objectList: [{listedString: "a"}, {listedString: "b"}, {listedString: "c"}],
+	nestedObject: {
+		innerString: "hi again"
+	}
 })
 
 const nullString = JSON.stringify({
-	"someString": null,
-	"someNumber": 1,
-	"someBoolean": true
+	someString: null,
+	someNumber: 1,
+	someBoolean: true,
+	stringList: ["hi", "yo", "whatup"],
+	objectList: [{listedString: "a"}, {listedString: "b"}, {listedString: "c"}],
+	nestedObject: {
+		innerString: "hi again"
+	}
 })
 
 describe("runtime-type-validation", () => {
