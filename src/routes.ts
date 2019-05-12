0bdb3b43fd65eba123b0b9f837db2991fc5d066f
Jonathan <coleji@bc.edu>
2019-05-12 12:18:52 -0400

    added a pageflow system, need to implement

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
-export default {
-    '/': {
-        title: 'Picker'
-    },
-    '/counter/:num': {
-        title: 'Counter'
-    }
-};
+import { PageFlowStateMachine } from "./core/PageFlowStateMachine";
+import {path as requiredInfoPath} from "./containers/registration/RequiredInfo"
+import {path as emergPath } from "./containers/registration/EmergencyContact"
+import {path as swimPath} from "./containers/registration/SwimProof"
+import {path as surveyPath} from "./containers/registration/SurveyInfo"
+
+const replacePersonId = (personId: number) => (path: string) => requiredInfoPath.replace(/:personId/, personId.toString())
+
+export const editInfo: (personId: number) => PageFlowStateMachine = (personId: number) => PageFlowStateMachine.fromRoutes([
+	"/",
+	replacePersonId(personId)(requiredInfoPath),
+	replacePersonId(personId)(emergPath),
+	replacePersonId(personId)(swimPath),
+	replacePersonId(personId)(surveyPath)
+])
